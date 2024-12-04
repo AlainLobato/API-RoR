bc22ad218f5da8fab4d0dd8c5b5666fe477add37
AlainLobato <lobatoa829@gmail.com>
2024-12-04 11:03:29 -0600

    created: API code

---
+++ db/migrate/20241204000933_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[7.2]
+  def change
+    create_table :tasks do |t|
+      t.string :task
+      t.boolean :complete
+      t.date :created_on
+      t.date :finished_on
+
+      t.timestamps
+    end
+  end
+end
